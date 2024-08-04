ecba387d406c7d3e9072d6e2a7954ddf6dce6294
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-05 04:00:57 +0700

    new: add GET endpoint and PUT endpoint

--- video-streaming-service/src/main/java/com/nlu/app/controller/FileController.java
+++ video-streaming-service/src/main/java/com/nlu/app/controller/FileController.java
@@ -1,19 +1,28 @@
 package com.nlu.app.controller;
-
-import com.amazonaws.services.s3.AmazonS3;
+import com.nlu.app.dto.AppResponse;
+import com.nlu.app.service.FileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+
 public class FileController {
-    AmazonS3 s3;
-    @GetMapping("aws/testc")
-    public void connect() {
-        System.out.println(s3);
+    FileService fileService;
+    @GetMapping("file")
+    public AppResponse<String> getFileSignedURL(@RequestParam String key) {
+        return AppResponse.<String>builder()
+                .result(fileService.generateURL(key))
+                .build();
+    }
+
+    @PutMapping("file/{fileName}")
+    public AppResponse<String> putFileSignedURL(@PathVariable String fileName) {
+        return AppResponse.<String>builder()
+                .result(fileService.signUpload(fileName))
+                .build();
     }
 }
