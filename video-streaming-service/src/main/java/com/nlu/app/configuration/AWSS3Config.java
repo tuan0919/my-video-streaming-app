50c8a96238858482736df72b0014f4d7315e5b67
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-05 03:59:35 +0700

    update: add bean class for S3Client and S3Presigner

--- video-streaming-service/src/main/java/com/nlu/app/configuration/AWSS3Config.java
+++ video-streaming-service/src/main/java/com/nlu/app/configuration/AWSS3Config.java
@@ -1,13 +1,14 @@
 package com.nlu.app.configuration;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class AWSS3Config {
@@ -19,11 +20,19 @@ public class AWSS3Config {
     String region;
 
     @Bean
-    public AmazonS3 getS3Client() {
-        final AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+    public S3Client getS3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create("nqat0919-uploader"))
+                .build();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public S3Presigner getS3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create("nqat0919-uploader"))
                 .build();
     }
 }
