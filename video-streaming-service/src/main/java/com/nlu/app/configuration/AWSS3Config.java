85f5c3c18d3a54c2213014d6841f141f20ad7173
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-04 21:00:04 +0700

    update: add configuration class to create AmazonS3Client.

--- video-streaming-service/src/main/java/com/nlu/app/configuration/AWSS3Config.java
+++ video-streaming-service/src/main/java/com/nlu/app/configuration/AWSS3Config.java
@@ -1,6 +1,12 @@
 package com.nlu.app.configuration;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,4 +18,12 @@ public class AWSS3Config {
     @Value("${amazon.aws.region}")
     String region;
 
+    @Bean
+    public AmazonS3 getS3Client() {
+        final AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
 }
