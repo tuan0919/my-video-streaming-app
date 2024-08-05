e6a4383f8006b6b7d454eb4a2f8b4b9957f91051
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-05 18:10:34 +0700

    new: add database entity

---
+++ video-streaming-service/src/main/java/com/nlu/app/entity/Video.java
@@ -0,0 +1,42 @@
+package com.nlu.app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+@Entity(name = "videos")
+@Builder
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String videoId;
+    @Column
+    String userId;
+    @Column(columnDefinition = "datetime default now()")
+    LocalDateTime createAt;
+    @Column(columnDefinition = "varchar(255) collate utf8mb4_unicode_ci not null")
+    String videoKey;
+    @Column(columnDefinition = "bigint not null")
+    Long duration;
+    @Column(columnDefinition = "varchar(255) collate utf8mb4_unicode_ci not null")
+    String thumbnailKey;
+    @Column
+    String tagsString;
+    @Transient @Getter(AccessLevel.NONE) @Setter(AccessLevel.NONE)
+    List<String> tags;
+    @PostLoad
+    private void loadTags() {
+        this.tags = Arrays.asList(tagsString.split(","));
+    }
+    @PrePersist
+    @PreUpdate
+    private void saveTags() {
+        this.tagsString = String.join(",", tags);
+    }
+}
