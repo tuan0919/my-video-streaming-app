63dd4ef15e879215a25d6f189379bb91c73caa4f
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-09 02:26:21 +0700

    update: small fix at application init config

--- identity-service/src/main/java/com/nlu/app/configuration/ApplicationInitConfig.java
+++ identity-service/src/main/java/com/nlu/app/configuration/ApplicationInitConfig.java
@@ -4,8 +4,10 @@ import java.util.HashSet;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.nlu.app.constant.PredefinedRole;
@@ -25,48 +27,40 @@ import lombok.extern.slf4j.Slf4j;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class ApplicationInitConfig {
-
+    UserRepository userRepository;
+    RoleRepository roleRepository;
     PasswordEncoder passwordEncoder;
-
     @NonFinal
     static final String ADMIN_USER_NAME = "admin";
-
     @NonFinal
     static final String ADMIN_PASSWORD = "admin";
-
-    @Bean
-    @ConditionalOnProperty(
-            prefix = "spring",
-            value = "datasource.driverClassName",
-            havingValue = "com.mysql.cj.jdbc.Driver")
-    ApplicationRunner applicationRunner(UserRepository userRepository, RoleRepository roleRepository) {
+    @EventListener(ApplicationReadyEvent.class)
+    void applicationRunner() {
         log.info("Initializing application.....");
-        return args -> {
-            if (userRepository.findByUsername(ADMIN_USER_NAME).isEmpty()) {
-                roleRepository.save(Role.builder()
-                        .name(PredefinedRole.USER_ROLE)
-                        .description("User role")
-                        .build());
+        if (userRepository.findByUsername(ADMIN_USER_NAME).isEmpty()) {
+            roleRepository.save(Role.builder()
+                    .name(PredefinedRole.USER_ROLE)
+                    .description("User role")
+                    .build());
 
-                Role adminRole = roleRepository.save(Role.builder()
-                        .name(PredefinedRole.ADMIN_ROLE)
-                        .description("Admin role")
-                        .build());
+            Role adminRole = roleRepository.save(Role.builder()
+                    .name(PredefinedRole.ADMIN_ROLE)
+                    .description("Admin role")
+                    .build());
 
-                var roles = new HashSet<Role>();
-                roles.add(adminRole);
+            var roles = new HashSet<Role>();
+            roles.add(adminRole);
 
-                User user = User.builder()
-                        .username(ADMIN_USER_NAME)
-                        .emailVerified(true)
-                        .password(passwordEncoder.encode(ADMIN_PASSWORD))
-                        .roles(roles)
-                        .build();
+            User user = User.builder()
+                    .username(ADMIN_USER_NAME)
+                    .emailVerified(true)
+                    .password(passwordEncoder.encode(ADMIN_PASSWORD))
+                    .roles(roles)
+                    .build();
 
-                userRepository.save(user);
-                log.warn("admin user has been created with default password: admin, please change it");
-            }
-            log.info("Application initialization completed .....");
-        };
+            userRepository.save(user);
+            log.warn("admin user has been created with default password: admin, please change it");
+        }
+        log.info("Application initialization completed .....");
     }
 }
