7be0e137489dec0d86ba9a370fa9f6a65e48eed8
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-09 02:25:11 +0700

    update: update config routes

--- gateway-service/src/main/java/com/nlu/app/route/Routes.java
+++ gateway-service/src/main/java/com/nlu/app/route/Routes.java
@@ -1,22 +1,47 @@
 package com.nlu.app.route;
+import com.nlu.app.filter.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 
 @Configuration
 public class Routes {
+    @Value("${identity-service.domain}")
+    String identityDomain;
+    @Value("${video-streaming-service.domain}")
+    String videoStreamingDomain;
+    @Value("${gateway-service./prefix}")
+    String prefix;
+    @Value("${gateway-service.strip}")
+    int PREFIX_STRIP;
+    public static final List<String> openEndpoints = List.of(
+            "/identity/auth/.*",
+            "/identity/users/registration"
+    );
+    AuthenticationFilter authenticationFilter;
+    @Autowired
+    public void setAuthenticationFilter(AuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        var authFilter = authenticationFilter
+                .apply(new AuthenticationFilter.Config());
         return builder.routes()
-                .route(r -> r.path("/api/identity/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8080")
+                .route(r -> r.path(prefix+"/identity/**")
+                        .filters(f -> f.stripPrefix(PREFIX_STRIP).filter(authFilter))
+                        .uri(identityDomain)
                 )
-                .route(r -> r.path("/api/file/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("http://wjbu.online:8888")
+                .route(r -> r.path(prefix+"/file/**")
+                        .filters(f -> f.stripPrefix(PREFIX_STRIP).filter(authFilter))
+                        .uri(videoStreamingDomain)
                 ).build();
     }
 }
