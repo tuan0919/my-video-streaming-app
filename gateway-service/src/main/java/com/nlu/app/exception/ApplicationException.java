b1534277d503128e69fb266bc7bcc2fcc8a01794
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-09 02:21:55 +0700

    new: add configuration folder for api gateway service

---
+++ gateway-service/src/main/java/com/nlu/app/exception/ApplicationException.java
@@ -0,0 +1,20 @@
+package com.nlu.app.exception;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class ApplicationException extends RuntimeException {
+    ErrorCode errorCode;
+
+    public ApplicationException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
