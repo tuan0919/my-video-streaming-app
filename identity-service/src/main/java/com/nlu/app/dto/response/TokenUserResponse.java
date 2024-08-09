1d08ab65a9a303b4e393f469f7a2b71be7d16ce7
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-10 03:45:27 +0700

    new: add new DTO

---
+++ identity-service/src/main/java/com/nlu/app/dto/response/TokenUserResponse.java
@@ -0,0 +1,16 @@
+package com.nlu.app.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TokenUserResponse {
+    String username;
+    List<String> roles;
+}
