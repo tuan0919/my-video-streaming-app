eb10df1be12f03fd44ea35d45b03418652605433
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-10 04:29:37 +0700

    new: add identity web client at video streaming service

--- video-streaming-service/src/main/java/com/nlu/app/repository/IdentityWebClient.java
+++ video-streaming-service/src/main/java/com/nlu/app/repository/IdentityWebClient.java
@@ -1,12 +1,14 @@
 package com.nlu.app.repository;
 
 import com.nlu.app.dto.AppResponse;
+import com.nlu.app.dto.webclient.identity.request.TokenUserRequest;
 import com.nlu.app.dto.webclient.identity.response.TokenUserResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.PostExchange;
+import reactor.core.publisher.Mono;
 
 public interface IdentityWebClient {
     @PostExchange(url = "identity/auth/userinfo", contentType = MediaType.APPLICATION_JSON_VALUE)
-    Mono<AppResponse<TokenUserResponse>> introspect(@RequestBody IntrospectRequest request);
+    Mono<AppResponse<TokenUserResponse>> userInfo(@RequestBody TokenUserRequest request);
 }
