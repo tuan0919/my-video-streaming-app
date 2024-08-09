ac6faf4e814e75ca73f4840b969c54e34a512a1f
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-10 04:30:30 +0700

    update: rework controller, now work with webflux instead

--- video-streaming-service/src/main/java/com/nlu/app/controller/FileController.java
+++ video-streaming-service/src/main/java/com/nlu/app/controller/FileController.java
@@ -1,11 +1,14 @@
 package com.nlu.app.controller;
 import com.nlu.app.dto.AppResponse;
+import com.nlu.app.dto.request.PutFileRequest;
+import com.nlu.app.dto.request.SaveFileRequest;
+import com.nlu.app.dto.response.SignedURLResponse;
 import com.nlu.app.service.FileService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/files")
@@ -14,35 +17,30 @@ import org.springframework.web.bind.annotation.*;
 public class FileController {
     FileService fileService;
 
+    @PostMapping
+    public Mono<AppResponse<String>> saveFile(@RequestBody SaveFileRequest request) {
+        return fileService.moveToInventory(request)
+                .map(response -> {
+                    return AppResponse.<String>builder()
+                            .result(response).build();
+                });
+    }
+
+    @PutMapping
+    public Mono<AppResponse<SignedURLResponse>> saveFile(@RequestBody PutFileRequest request) {
+        return fileService.uploadToTemp(request)
+                .map(response -> {
+                    return AppResponse.<SignedURLResponse>builder()
+                            .result(response).build();
+                });
+    }
+
     @GetMapping
-    public AppResponse<String> getFileSignedURL(@RequestParam(value = "key", required = true) String key) {
-        return AppResponse.<String>builder()
-                .result(fileService.generateURL(key))
-                .build();
-    }
-
-    @GetMapping("sign-cookies")
-    public AppResponse<String> getFileSignedURL(HttpServletResponse response) {
-        var cookies = fileService.signCookies();
-        response.addHeader("Set-Cookie", cookies.policyHeaderValue() + "; Domain=.wjbu.online; HttpOnly");
-        response.addHeader("Set-Cookie", cookies.keyPairIdHeaderValue() + "; Domain=.wjbu.online; HttpOnly");
-        response.addHeader("Set-Cookie", cookies.signatureHeaderValue() + "; Domain=.wjbu.online; HttpOnly");
-        return AppResponse.<String>builder()
-                .result("OK")
-                .build();
-    }
-
-    @PutMapping("/{fileName}")
-    public AppResponse<String> putFileSignedURL(@PathVariable String fileName) {
-        return AppResponse.<String>builder()
-                .result(fileService.uploadToTemp(fileName))
-                .build();
-    }
-
-    @PostMapping("/{key}")
-    public AppResponse<String> saveFile(@PathVariable String key) {
-        return AppResponse.<String>builder()
-                .result(fileService.moveToInventory(key))
-                .build();
+    public Mono<AppResponse<SignedURLResponse>> getFile(@RequestParam("key") String key) {
+        return fileService.generateURL(key)
+                .map(response -> {
+                    return AppResponse.<SignedURLResponse>builder()
+                            .result(response).build();
+                });
     }
 }
