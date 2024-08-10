d74be6514f416bc16a2175e78edc0202f4696483
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-10 11:45:12 +0700

    update: add new endpoint to open endpoints.

--- identity-service/src/main/java/com/nlu/app/configuration/SecurityConfig.java
+++ identity-service/src/main/java/com/nlu/app/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private static final String[] PUBLIC_ENDPOINTS = {
-        "/users/registration", "/auth/token", "/auth/introspect", "/auth/logout", "/auth/refresh"
+        "/users/registration", "/auth/token", "/auth/introspect", "/auth/logout", "/auth/refresh", "/auth/userinfo"
     };
 
     private final CustomJwtDecoder customJwtDecoder;
