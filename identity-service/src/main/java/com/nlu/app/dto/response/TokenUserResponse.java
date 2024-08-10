35a73ac7c56a3a5b41d51268cc57e1e065950caa
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-10 11:43:34 +0700

    update: add new field to DTO

--- identity-service/src/main/java/com/nlu/app/dto/response/TokenUserResponse.java
+++ identity-service/src/main/java/com/nlu/app/dto/response/TokenUserResponse.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TokenUserResponse {
+    String userId;
     String username;
     List<String> roles;
 }
