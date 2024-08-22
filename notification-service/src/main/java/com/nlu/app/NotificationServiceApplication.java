abf3bee6b02699d9a439a97c3818b2c895c02890
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-22 21:30:58 +0700

    refactor: typo

--- notification-service/src/main/java/com/nlu/app/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/nlu/app/NotificationServiceApplication.java
@@ -1,13 +1,27 @@
 package com.nlu.app;
 
+import com.nlu.app.common.event.UserCreationEvent;
+import com.nlu.app.constant.NotificationType;
+import com.nlu.app.entity.Notification;
+import com.nlu.app.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
-
 }
