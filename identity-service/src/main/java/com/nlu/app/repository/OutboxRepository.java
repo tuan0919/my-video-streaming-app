ca6a38e2ffc291ae4585ee15aa43431aa3d50a9f
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-26 18:01:30 +0700

    update: small fix at repository

---
+++ identity-service/src/main/java/com/nlu/app/repository/OutboxRepository.java
@@ -0,0 +1,7 @@
+package com.nlu.app.repository;
+
+import com.nlu.app.entity.Outbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OutboxRepository extends JpaRepository<Outbox, String> {
+}
