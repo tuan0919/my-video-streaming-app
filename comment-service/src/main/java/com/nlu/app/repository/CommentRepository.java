3747b444813feb24fc381e2f0a17d6ef46f9526a
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-27 21:24:19 +0700

    update: small changes at comment repository

--- comment-service/src/main/java/com/nlu/app/repository/CommentRepository.java
+++ comment-service/src/main/java/com/nlu/app/repository/CommentRepository.java
@@ -3,9 +3,8 @@ package com.nlu.app.repository;
 import com.nlu.app.entity.Comment;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface CommentRepository extends ReactiveMongoRepository<Comment, String> {
-    Flux<Comment> findByVideoId(String videoId);
-    Flux<Comment> findByUserId(String userId);
-    Flux<Comment> findByParentId(String parentId); // Truy vấn các reply theo comment cha
+
 }
