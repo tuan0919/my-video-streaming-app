01cacd844d3159a372d9011780c9e9465e5333ca
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-27 21:24:02 +0700

    update: small change about error code

--- comment-service/src/main/java/com/nlu/app/exception/ErrorCode.java
+++ comment-service/src/main/java/com/nlu/app/exception/ErrorCode.java
@@ -9,7 +9,6 @@ public enum ErrorCode {
     UNKNOWN_EXCEPTION(9999, "Something went wrong :(.", HttpStatus.INTERNAL_SERVER_ERROR),
     UNAUTHENTICATED(1002, "You are not authenticated!", HttpStatus.FORBIDDEN),
     UNAUTHORIZED(1003, "You are not authorized to access!", HttpStatus.UNAUTHORIZED),
-    RESOURCE_NOT_FOUND(1001, "Resource not found, please check again!", HttpStatus.NOT_FOUND),
     ;
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
