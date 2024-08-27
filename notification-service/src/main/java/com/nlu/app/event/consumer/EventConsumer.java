b43854fff3e9e7843fa847740919530619b5b116
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-27 21:20:43 +0700

    update: small change at consumer

--- notification-service/src/main/java/com/nlu/app/event/consumer/EventConsumer.java
+++ notification-service/src/main/java/com/nlu/app/event/consumer/EventConsumer.java
@@ -2,6 +2,7 @@ package com.nlu.app.event.consumer;
 
 import com.nlu.app.common.dto.UserCreationDTO;
 import com.nlu.app.common.event.UserCreationEvent;
+import com.nlu.app.common.event.comment_created.CommentCreationEvent;
 import com.nlu.app.constant.NotificationType;
 import com.nlu.app.entity.Notification;
 import com.nlu.app.service.NotificationService;
@@ -26,15 +27,28 @@ public class EventConsumer {
     public Function<Flux<Message<UserCreationEvent>>, Flux<Notification>> userCreationEvent() {
         return flux -> flux.flatMap(message -> {
             MessageHeaderAccessor accessor = new MessageHeaderAccessor(message);
-            log.info("userid in message[%s]'s header: %s",
-                    message.getHeaders().getId().toString(),
-                    accessor.getHeader("userid"));
+            log.info("userid in message[{}]'s header: {}", message.getHeaders().getId(), accessor.getHeader("userid"));
             var event = message.getPayload();
             Notification notification = new Notification();
             notification.setType(NotificationType.INFO);
             notification.setContent(String.format("Hello user %s, welcome to our services.", event.getUserCreateDTO().getUsername()));
             notification.setTime(event.getTimestamp());
             notification.setUserId(event.getUserCreateDTO().getUserId());
+            return service.insertDB(notification);
+        });
+    }
+
+    @Bean
+    public Function<Flux<Message<CommentCreationEvent>>, Flux<Notification>> commentRepliedEvent() {
+        return flux -> flux.flatMap(message -> {
+            MessageHeaderAccessor accessor = new MessageHeaderAccessor(message);
+            log.info("userid in message[{}]'s header: {}", message.getHeaders().getId(), accessor.getHeader("userid"));
+            var event = message.getPayload();
+            Notification notification = new Notification();
+            notification.setType(NotificationType.INFO);
+            notification.setContent(String.format("User with id %s has replied to your comment.", event.getDto().getReplierId()));
+            notification.setTime(event.getTimestamp());
+            notification.setUserId(event.getDto().getUserId());
             return service.insertDB(notification);
         });
     }
