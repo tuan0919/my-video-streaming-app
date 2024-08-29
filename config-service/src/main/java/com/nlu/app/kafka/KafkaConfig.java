f667ccb777b9041a08289b4efd30c0066482f4e7
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-29 20:16:15 +0700

    new: add config service

---
+++ config-service/src/main/java/com/nlu/app/kafka/KafkaConfig.java
@@ -0,0 +1,37 @@
+package com.nlu.app.kafka;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Properties;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic user_created() {
+        return TopicBuilder.name("user_created")
+                .partitions(5)
+                .replicas(3) // Number of replicas
+                .build();
+    }
+
+    @Bean
+    public NewTopic comment_replied() {
+        return TopicBuilder.name("comment_replied")
+                .partitions(5)
+                .replicas(3) // Number of replicas
+                .build();
+    }
+
+    @Bean
+    public NewTopic notification_created() {
+        return TopicBuilder.name("notification_created")
+                .partitions(5)
+                .replicas(3) // Number of replicas
+                .build();
+    }
+}
