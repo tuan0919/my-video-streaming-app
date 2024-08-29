084a651ecb5e509bb65a1a8c94d066d962d5c795
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-29 21:35:45 +0700

    update: small update at consumer

--- notification-service/src/main/java/com/nlu/app/event/consumer/EventConsumer.java
+++ notification-service/src/main/java/com/nlu/app/event/consumer/EventConsumer.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import reactor.core.publisher.Flux;
 
@@ -24,32 +25,40 @@ public class EventConsumer {
     private final NotificationService service;
 
     @Bean
-    public Function<Flux<Message<UserCreationEvent>>, Flux<Notification>> userCreationEvent() {
+    public Function<Flux<Message<UserCreationEvent>>, Flux<Message<Notification>>> userCreationEvent() {
         return flux -> flux.flatMap(message -> {
             MessageHeaderAccessor accessor = new MessageHeaderAccessor(message);
             log.info("userid in message[{}]'s header: {}", message.getHeaders().getId(), accessor.getHeader("userid"));
             var event = message.getPayload();
+            String userId = event.getUserCreateDTO().getUserId();
             Notification notification = new Notification();
             notification.setType(NotificationType.INFO);
             notification.setContent(String.format("Hello user %s, welcome to our services.", event.getUserCreateDTO().getUsername()));
             notification.setTime(event.getTimestamp());
-            notification.setUserId(event.getUserCreateDTO().getUserId());
-            return service.insertDB(notification);
+            notification.setUserId(userId);
+            return service.insertDB(notification)
+                    .map(msg -> MessageBuilder.withPayload(msg)
+                            .setHeader("partitonKey", userId)
+                            .build());
         });
     }
 
     @Bean
-    public Function<Flux<Message<CommentCreationEvent>>, Flux<Notification>> commentRepliedEvent() {
+    public Function<Flux<Message<CommentCreationEvent>>, Flux<Message<Notification>>> commentRepliedEvent() {
         return flux -> flux.flatMap(message -> {
             MessageHeaderAccessor accessor = new MessageHeaderAccessor(message);
             log.info("userid in message[{}]'s header: {}", message.getHeaders().getId(), accessor.getHeader("userid"));
             var event = message.getPayload();
+            String userId = event.getDto().getUserId();
             Notification notification = new Notification();
             notification.setType(NotificationType.INFO);
             notification.setContent(String.format("User with id %s has replied to your comment.", event.getDto().getReplierId()));
             notification.setTime(event.getTimestamp());
-            notification.setUserId(event.getDto().getUserId());
-            return service.insertDB(notification);
+            notification.setUserId(userId);
+            return service.insertDB(notification)
+                    .map(msg -> MessageBuilder.withPayload(msg)
+                            .setHeader("partitonKey", userId)
+                            .build());
         });
     }
 }
