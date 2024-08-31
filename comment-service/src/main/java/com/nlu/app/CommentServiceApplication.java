c61175bbe554eb2b7365c784b18c6987e0fd5c57
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-01 03:57:25 +0700

    update: add @EnableDiscovery annotation on main

--- comment-service/src/main/java/com/nlu/app/CommentServiceApplication.java
+++ comment-service/src/main/java/com/nlu/app/CommentServiceApplication.java
@@ -2,12 +2,16 @@ package com.nlu.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommentServiceApplication.class, args);
 	}
-
 }
--- identity-service/src/main/java/com/nlu/app/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/nlu/app/IdentityServiceApplication.java
@@ -2,12 +2,13 @@ package com.nlu.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 public class IdentityServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IdentityServiceApplication.class, args);
     }
+
 }
