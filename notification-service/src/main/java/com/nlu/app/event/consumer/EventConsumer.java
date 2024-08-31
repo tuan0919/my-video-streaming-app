a557d6403a48e443ab9220a20faf78e2c5ad3c23
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-31 20:18:50 +0700

    update: change logic in consumer, now using imparative way instead of reactive codes

--- notification-service/src/main/java/com/nlu/app/event/consumer/EventConsumer.java
+++ notification-service/src/main/java/com/nlu/app/event/consumer/EventConsumer.java
@@ -7,90 +7,67 @@ import com.nlu.app.entity.Notification;
 import com.nlu.app.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.stream.binder.BinderHeaders;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.kafka.receiver.ReceiverOffset;
-import reactor.util.retry.Retry;
 
-import java.time.Duration;
-import java.util.UUID;
-import java.util.function.Function;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.function.Consumer;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class EventConsumer {
-    private final StreamBridge streamBridge;
     private final NotificationService service;
 
     @Bean
-    public Function<Flux<Message<UserCreationEvent>>, Flux<Message<Notification>>> userCreationEvent() {
-        return flux -> flux.flatMap(message -> {
-            MessageHeaderAccessor accessor = new MessageHeaderAccessor(message);
-            log.info("userid in message[{}]'s header: {}", message.getHeaders().getId(), accessor.getHeader("userid"));
+    public Consumer<Message<UserCreationEvent>> userCreationEvent() {
+        return message -> {
             var event = message.getPayload();
             String userId = event.getUserCreateDTO().getUserId();
             Notification notification = new Notification();
-            ReceiverOffset ack = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, ReceiverOffset.class);
+            Acknowledgment ack = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
             notification.setType(NotificationType.INFO);
             notification.setContent(String.format("Hello user %s, welcome to our services.", event.getUserCreateDTO().getUsername()));
-            notification.setTime(event.getTimestamp());
+            var dateTime = Instant.ofEpochMilli(event.getUserCreateDTO().getTimestamp())
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+            notification.setTime(dateTime);
             notification.setUserId(userId);
-            UUID messageId = message.getHeaders().getId();
-            return service.insertDB(notification)
-                    .retryWhen(Retry.backoff(5, Duration.ofSeconds(1))
-                            .onRetryExhaustedThrow((_, retrySignal) -> {
-                                // Log retry information
-                                long retryCount = retrySignal.totalRetries(); // Tổng số lần retry đã thực hiện
-                                log.warn("Retry attempt: {} for message [{}]" ,retryCount, messageId);
-                                return retrySignal.failure();
-                            }).maxBackoff(Duration.ofSeconds(10)))
-                    .map(msg ->  {
-                        ack.acknowledge();
-                        return MessageBuilder.withPayload(msg)
-                                .setHeader("myKey", "ABC")
-                                .build();
-                    })
-                    .onErrorResume(error -> {
-                        error.printStackTrace();
-                        log.error("given up on message [{}]", messageId);
-                        streamBridge.send("userCreationEvent-dlq", MessageBuilder.withPayload(event)
-                                .setHeader("myKey", "ABC")
-                                .build());
-                        ack.acknowledge();
-                        return Mono.empty();
-                    });
-        });
+            try {
+                service.insertDB(notification);
+                ack.acknowledge();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 
     @Bean
-    public Function<Flux<Message<CommentCreationEvent>>, Flux<Message<Notification>>> commentRepliedEvent() {
-        return flux -> flux.flatMap(message -> {
-            MessageHeaderAccessor accessor = new MessageHeaderAccessor(message);
-            log.info("userid in message[{}]'s header: {}", message.getHeaders().getId(), accessor.getHeader("userid"));
+    public Consumer<Message<CommentCreationEvent>> commentRepliedEvent() {
+        return message -> {
             var event = message.getPayload();
             String userId = event.getDto().getUserId();
             Notification notification = new Notification();
+            Acknowledgment ack = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
             notification.setType(NotificationType.INFO);
-            ReceiverOffset ack = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, ReceiverOffset.class);
-            notification.setContent(String.format("User with id %s has replied to your comment.", event.getDto().getReplierId()));
-            notification.setTime(event.getTimestamp());
+            notification.setContent(String.format("User with id %s replied your comment.", event.getDto().getUserId()));
+            var dateTime = Instant.ofEpochMilli(event.getTimestamp())
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+            notification.setTime(dateTime);
             notification.setUserId(userId);
-            return service.insertDB(notification)
-                    .map(msg ->  {
-                        ack.acknowledge();
-                        return MessageBuilder.withPayload(msg)
-                                .setHeader("partitonKey", userId)
-                                .build();
-                    });
-        });
+            try {
+                service.insertDB(notification);
+                ack.acknowledge();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
