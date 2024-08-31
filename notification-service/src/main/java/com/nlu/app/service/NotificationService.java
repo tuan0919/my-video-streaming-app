a02165ef0a8dcdc47918e19290c132a205165644
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-31 20:17:19 +0700

    update: change service logic, apply inserting to outbox repository

--- notification-service/src/main/java/com/nlu/app/service/NotificationService.java
+++ notification-service/src/main/java/com/nlu/app/service/NotificationService.java
@@ -1,18 +1,37 @@
 package com.nlu.app.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.nlu.app.entity.Notification;
+import com.nlu.app.entity.Outbox;
 import com.nlu.app.repository.NotificationRepository;
+import com.nlu.app.repository.OutboxRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository repository;
+    private final OutboxRepository outboxRepository;
 
-    public Mono<Notification> insertDB(Notification notification) {
-//        return repository.insert(notification);
-        return Mono.error(new Exception("\"Cannot consume message\""));
+    @Transactional
+    public Notification insertDB(Notification notification) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        var entity = repository.save(notification);
+        var outbox = new Outbox();
+        try {
+            outbox.setPayload(objectMapper.writeValueAsString(entity));
+            outbox.setType("insert");
+            outbox.setAggregateType("created");
+            outbox.setAggregateId(entity.getUserId());
+            outboxRepository.save(outbox);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return entity;
     }
 }
