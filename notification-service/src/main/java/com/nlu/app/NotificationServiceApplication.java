3bd245d8166fa5221a0affbedc07edb3b6010557
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-01 03:56:39 +0700

    update: add @EnableDiscovery annotation on main

--- notification-service/src/main/java/com/nlu/app/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/nlu/app/NotificationServiceApplication.java
@@ -1,27 +1,25 @@
 package com.nlu.app;
 
-import com.nlu.app.common.event.UserCreationEvent;
-import com.nlu.app.constant.NotificationType;
-import com.nlu.app.entity.Notification;
-import com.nlu.app.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.function.Consumer;
-import java.util.function.Function;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-@Slf4j
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
