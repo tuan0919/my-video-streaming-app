650dcda32b0527477b2d8b59aaee532a16f8bcfb
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-02 03:03:06 +0700

    update: add new annotation for enabling eureka discovery client

--- notification-service/src/main/java/com/nlu/app/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/nlu/app/NotificationServiceApplication.java
@@ -1,7 +1,5 @@
 package com.nlu.app;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
--- video-streaming-service/src/main/java/com/nlu/app/VideoStreamingServiceApplication.java
+++ video-streaming-service/src/main/java/com/nlu/app/VideoStreamingServiceApplication.java
@@ -2,8 +2,10 @@ package com.nlu.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VideoStreamingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VideoStreamingServiceApplication.class, args);
