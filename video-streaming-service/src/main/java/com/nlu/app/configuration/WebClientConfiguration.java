d8dea0fe78c2c6fc0dea2a3187f4059e82708388
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-02 03:04:16 +0700

    update: adding @LoadBalanced annotation in WebClient Bean

--- video-streaming-service/src/main/java/com/nlu/app/configuration/WebClientConfiguration.java
+++ video-streaming-service/src/main/java/com/nlu/app/configuration/WebClientConfiguration.java
@@ -1,20 +1,32 @@
 package com.nlu.app.configuration;
 
 import com.nlu.app.repository.IdentityWebClient;
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfiguration {
     @Value("${identity-service.domain}")
     String identityBaseURI;
     @Bean
-    WebClient webClient() {
-        return WebClient.builder()
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    WebClient webClient(WebClient.Builder builder) {
+        HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+        return builder
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl(identityBaseURI)
                 .build();
     }
