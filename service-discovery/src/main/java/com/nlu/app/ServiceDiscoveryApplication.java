7137c3597936ff4115057e61d19e5cbf498d368e
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-05 02:43:00 +0700

    update: small test beans in main class, will be removed later (#1)

--- service-discovery/src/main/java/com/nlu/app/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/nlu/app/ServiceDiscoveryApplication.java
@@ -1,15 +1,30 @@
 package com.nlu.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceDiscoveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
 
+}
+
+@RestController
+@Slf4j
+class HomeController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		var greeting = "Hello " + name;
+		log.info("Greeting: {}", greeting);
+		return greeting;
+	}
 }
