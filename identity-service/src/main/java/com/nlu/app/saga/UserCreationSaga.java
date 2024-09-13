4c075c8a8a6a1b869cd18a4bea8b82e351c63bf4
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-13 20:49:03 +0700

    new: upload new folder saga for identity service

---
+++ identity-service/src/main/java/com/nlu/app/saga/UserCreationSaga.java
@@ -0,0 +1,29 @@
+package com.nlu.app.saga;
+
+import com.nlu.app.framework.Saga;
+
+public class UserCreationSaga extends Saga {
+
+    public static final String IDENTITY_CREATION = "userCreation";
+
+    public static final String NOTIFICATION_CREATION = "notificationCreation";
+
+    public static final String[] STATE_ORDER = {
+            IDENTITY_CREATION,
+            NOTIFICATION_CREATION
+    };
+
+    public void init() {
+        goNext();
+    }
+
+    @Override
+    public void goNext() {
+
+    }
+
+    @Override
+    public void goBack() {
+
+    }
+}
