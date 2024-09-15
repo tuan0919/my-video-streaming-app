23c7db472863155e3b46668138aa9c87dbeea44a
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-16 02:44:43 +0700

    new: upload handler in identity service

---
+++ identity-service/src/main/java/com/nlu/app/handler/UserEventsHandler.java
@@ -0,0 +1,33 @@
+package com.nlu.app.handler;
+
+import com.nlu.app.entity.User;
+import com.nlu.app.events.UserCreateEvent;
+import com.nlu.app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class UserEventsHandler {
+    private final UserRepository userRepository;
+    @Transactional
+    public Optional<User> onUserCreateEvent(UserCreateEvent event) {
+        boolean isExisted = userRepository.findById(event.getUserId()).isPresent();
+        if (!isExisted) {
+            User user = new User();
+            user.setId(event.getUserId());
+            user.setUsername(event.getUsername());
+            user.setPassword(event.getPassword());
+            user.setEmail(event.getEmail());
+            user.setRoles(Set.of(event.getRole()));
+            userRepository.save(user);
+            return Optional.of(user);
+        }
+        return Optional.empty();
+    }
+}
