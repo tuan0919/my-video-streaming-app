7c237654921352beed26f823e970d069d5f42389
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-19 18:10:47 +0700

    new: add service package for command side

---
+++ identity-service/src/main/java/com/nlu/app/commandSide/service/UserService.java
@@ -0,0 +1,32 @@
+package com.nlu.app.commandSide.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nlu.app.commandSide.commands.CreateUserCommand;
+import com.nlu.app.querySide.dto.request.UserCreationRequest;
+
+@Service
+public class UserService {
+    private CommandGateway commandGateway;
+
+    @Autowired
+    public void setCommandGateway(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    public CompletableFuture<String> createUser(UserCreationRequest request) {
+        var command = CreateUserCommand.builder()
+                .dob(request.getDob())
+                .email(request.getEmail())
+                .city(request.getCity())
+                .username(request.getUsername())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .build();
+        return commandGateway.send(command).thenApply(x -> "OK");
+    }
+}
