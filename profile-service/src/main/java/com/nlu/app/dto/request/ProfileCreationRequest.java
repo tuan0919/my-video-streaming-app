954626b5c76d293bcdf3bab60c3bccea30595d75
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-25 14:27:19 +0700

    new: add dto package to profile service

---
+++ profile-service/src/main/java/com/nlu/app/dto/request/ProfileCreationRequest.java
@@ -0,0 +1,16 @@
+package com.nlu.app.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProfileCreationRequest {
+    String userId;
+    String fullName;
+    String country;
+    String bio;
+}
---
+++ profile-service/src/main/java/com/nlu/app/dto/response/ProfileCreationResponse.java
@@ -0,0 +1,16 @@
+package com.nlu.app.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProfileCreationResponse {
+    String userId;
+    String fullName;
+    String country;
+    String bio;
+}
