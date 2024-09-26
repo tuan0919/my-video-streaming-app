cafd871ac2e2886a07860f8a2e2a943fe451ae58
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-26 19:25:15 +0700

    new: addd web client repository

--- identity-service/src/main/java/com/nlu/app/repository/webclient/IdentityWebClient.java
+++ identity-service/src/main/java/com/nlu/app/repository/webclient/IdentityWebClient.java
@@ -1,14 +1,16 @@
 package com.nlu.app.repository.webclient;
 
-import com.nlu.app.dto.AppResponse;
-import com.nlu.app.dto.request.IntrospectRequest;
-import com.nlu.app.dto.response.IntrospectResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.PostExchange;
+
+import com.nlu.app.dto.AppResponse;
+import com.nlu.app.dto.webclient.profile.request.ProfileCreationRequest;
+import com.nlu.app.dto.webclient.profile.response.ProfileCreationResponse;
+
 import reactor.core.publisher.Mono;
 
 public interface IdentityWebClient {
     @PostExchange(url = "/profile", contentType = MediaType.APPLICATION_JSON_VALUE)
-    Mono<AppResponse<IntrospectResponse>> introspect(@RequestBody IntrospectRequest request);
+    Mono<AppResponse<ProfileCreationResponse>> createProfile(@RequestBody ProfileCreationRequest request);
 }
