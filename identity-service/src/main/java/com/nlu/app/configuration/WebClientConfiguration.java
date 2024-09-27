029d41662893475b19b2e6081a22c8e38dbda047
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-27 21:24:13 +0700

    test: add web client configuration for identity service

---
+++ identity-service/src/main/java/com/nlu/app/configuration/WebClientConfiguration.java
@@ -0,0 +1,39 @@
+package com.nlu.app.configuration;
+
+import com.nlu.app.repository.webclient.ProfileWebClient;
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebClientConfiguration {
+    @Value("${profile-service.domain}")
+    String identityBaseURI;
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    WebClient webClient(WebClient.Builder builder) {
+        HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+        return builder
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .baseUrl(identityBaseURI)
+                .build();
+    }
+    @Bean
+    ProfileWebClient profileWebClient(WebClient webClient) {
+        var factory = HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(webClient)).build();
+        return factory.createClient(ProfileWebClient.class);
+    }
+}
