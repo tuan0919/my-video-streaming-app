3a1062d539afdfd9c39caee3bb39068a4c8d806e
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-28 16:41:47 +0700

    new: add ProfileWebClient

--- identity-service/src/main/java/com/nlu/app/repository/webclient/ProfileWebClient.java
+++ identity-service/src/main/java/com/nlu/app/repository/webclient/ProfileWebClient.java
@@ -1,16 +1,15 @@
 package com.nlu.app.repository.webclient;
 
+import com.nlu.app.common.share.dto.profile_service.request.ProfileCreationRequest;
+import com.nlu.app.common.share.dto.profile_service.response.ProfileCreationResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.PostExchange;
-
 import com.nlu.app.dto.AppResponse;
-import com.nlu.app.dto.webclient.profile.request.ProfileCreationRequest;
-import com.nlu.app.dto.webclient.profile.response.ProfileCreationResponse;
 
 import reactor.core.publisher.Mono;
 
-public interface IdentityWebClient {
-    @PostExchange(url = "/profile", contentType = MediaType.APPLICATION_JSON_VALUE)
+public interface ProfileWebClient {
+    @PostExchange(url = "profile/users/internal/new", contentType = MediaType.APPLICATION_JSON_VALUE)
     Mono<AppResponse<ProfileCreationResponse>> createProfile(@RequestBody ProfileCreationRequest request);
 }
