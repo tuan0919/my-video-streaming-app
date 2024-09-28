d038e7ddb005922b61305bd0eb5f139552e23ab0
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-28 16:41:20 +0700

    new: add saga classes

--- identity-service/src/main/java/com/nlu/app/saga/IdentityManagementSaga.java
+++ identity-service/src/main/java/com/nlu/app/saga/IdentityManagementSaga.java
@@ -1,47 +1,89 @@
 package com.nlu.app.saga;
-
-import java.util.UUID;
-
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.modelling.saga.EndSaga;
-import org.axonframework.modelling.saga.SagaEventHandler;
-import org.axonframework.modelling.saga.SagaLifecycle;
-import org.axonframework.modelling.saga.StartSaga;
-import org.axonframework.spring.stereotype.Saga;
+import java.time.Duration;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nlu.app.common.share.dto.notification_service.request.NotificationCreationRequest;
+import com.nlu.app.common.share.dto.profile_service.request.ProfileCreationRequest;
+import com.nlu.app.common.share.event.NotificationCreatedEvent;
+import com.nlu.app.common.share.event.ProfileCreatedEvent;
+import com.nlu.app.repository.webclient.NotificationWebClient;
+import com.nlu.app.repository.webclient.ProfileWebClient;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import com.nlu.app.common.share.event.UserCreatedEvent;
 
-import com.nlu.app.common.axon.event.NotificationCreatedEvent;
-import com.nlu.app.common.axon.command.NotificationCreationCommand;
-import com.nlu.app.common.axon.event.UserCreatedEvent;
-
-@Saga
 @Component
-public class UserCreationSaga {
-    private transient CommandGateway commandGateway;
+@NoArgsConstructor
+@Slf4j
+public class IdentityManagementSaga {
+    ProfileWebClient profileWebClient;
+    NotificationWebClient notificationWebClient;
+    ObjectMapper objectMapper;
 
     @Autowired
-    public void setCommandGateway(CommandGateway commandGateway) {
-        this.commandGateway = commandGateway;
+    public void setProfileWebClient(ProfileWebClient profileWebClient) {
+        this.profileWebClient = profileWebClient;
     }
 
-    @StartSaga
-    @SagaEventHandler(associationProperty = "userId")
-    public void handle(UserCreatedEvent event) {
-        String notificationId = UUID.randomUUID().toString();
-        SagaLifecycle.associateWith("notificationId", notificationId);
-        var notificationCommand = NotificationCreationCommand.builder()
+    @Autowired
+    public void setNotificationWebClient(NotificationWebClient notificationWebClient) {
+        this.notificationWebClient = notificationWebClient;
+    }
+
+    @Autowired
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener(topics = "user_created", groupId = "identity-service")
+    public void userCreated(@Payload String payload, Acknowledgment ack) throws JsonProcessingException {
+        log.info("Saga user start: {}", payload);
+        UserCreatedEvent event = objectMapper.readValue(payload, UserCreatedEvent.class);
+        var profileCreate = ProfileCreationRequest.builder()
+                .bio("none")
                 .userId(event.getUserId())
-                .notificationId(notificationId)
-                .content("Welcome user " + event.getUsername())
+                .country("vn")
+                .fullName("")
                 .build();
-        commandGateway.send(notificationCommand); // create notification
+        try {
+            profileWebClient.createProfile(profileCreate).block();
+            ack.acknowledge();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @SagaEventHandler(associationProperty = "notificationId")
-    @EndSaga
-    public void handle(NotificationCreatedEvent event) {
-        // Process notification
-        System.out.println("Notificaton Created");
+    @KafkaListener(topics = "notification_created", groupId = "identity-service")
+    public void notificationCreated(@Payload String payload, Acknowledgment ack) throws JsonProcessingException {
+        NotificationCreatedEvent event = objectMapper.readValue(payload, NotificationCreatedEvent.class);
+        try {
+            // TODO: do something to announce end this saga
+            log.info("notification created: {}", event);
+            ack.acknowledge();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @KafkaListener(topics = "profile_created", groupId = "identity-service")
+    public void profileCreated(@Payload String payload, Acknowledgment ack) throws JsonProcessingException {
+        ProfileCreatedEvent event = objectMapper.readValue(payload, ProfileCreatedEvent.class);
+        var createNotification = NotificationCreationRequest.builder()
+                .type("INFO")
+                .userId(event.getUserId())
+                .content(String.format("Welcome userId %s to our service", event.getUserId()))
+                .build();
+        log.info("profile created: {}", event);
+        try {
+            notificationWebClient.createNotification(createNotification).block();
+            ack.acknowledge();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
