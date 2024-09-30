63931e546c340dfda91bf8e5196dddc464168532
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-30 08:43:09 +0700

    new: add outbox entity for services

---
+++ profile-service/src/main/java/com/nlu/app/entity/Outbox.java
@@ -0,0 +1,29 @@
+package com.nlu.app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity(name = "outbox")
+public class Outbox {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id = UUID.randomUUID().toString();
+    @Column(name = "aggregatetype")
+    String aggregateType;
+    @Column(name = "aggregateid")
+    String aggregateId;
+    String sagaAction;
+    String sagaStep;
+    Boolean sagaStepStatus;
+    String sagaId;
+    String payload;
+}
