82e02276099c216b6cd1a51020e2a6e71c0d67ee
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-09-30 21:25:04 +0700

    new: add removing event

---
+++ common-dtos/src/main/java/com/nlu/app/common/share/event/NotificationRemovedEvent.java
@@ -0,0 +1,13 @@
+package com.nlu.app.common.share.event;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.FieldDefaults;
+
+@Builder
+@Value
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class NotificationRemovedEvent {
+    String notificationId;
+}
