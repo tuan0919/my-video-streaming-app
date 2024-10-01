7e00caae621615e905ba51d9360f33d0eb3f0375
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-01 19:18:03 +0700

    new: add new steps

--- common-dtos/src/main/java/com/nlu/app/common/share/SagaAction.java
+++ common-dtos/src/main/java/com/nlu/app/common/share/SagaAction.java
@@ -2,4 +2,6 @@ package com.nlu.app.common.share;
 
 public class SagaAction {
     public static final String CREATE_NEW_USER = "CREATE_NEW_USER";
+    public static final String UPDATE_IDENTITY = "UPDATE_IDENTITY";
+    public static final String REMOVE_IDENTITY = "REMOVE_IDENTITY";
 }
--- common-dtos/src/main/java/com/nlu/app/common/share/SagaAdvancedStep.java
+++ common-dtos/src/main/java/com/nlu/app/common/share/SagaAdvancedStep.java
@@ -4,4 +4,5 @@ public class SagaAdvancedStep {
     public static final String IDENTITY_CREATE = "IDENTITY_CREATE";
     public static final String PROFILE_CREATE = "PROFILE_CREATE";
     public static final String NOTIFICATION_CREATE = "NOTIFICATION_CREATE";
+    public static final String IDENTITY_UPDATE = "IDENTITY_UPDATE";
 }
