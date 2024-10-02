5f6a789b28b452e0b10a0f8749d56900ffcde8eb
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-02 20:31:40 +0700

    new: new saga acton

--- common-dtos/src/main/java/com/nlu/app/common/share/SagaAction.java
+++ common-dtos/src/main/java/com/nlu/app/common/share/SagaAction.java
@@ -4,4 +4,5 @@ public class SagaAction {
     public static final String CREATE_NEW_USER = "CREATE_NEW_USER";
     public static final String UPDATE_IDENTITY = "UPDATE_IDENTITY";
     public static final String REMOVE_IDENTITY = "REMOVE_IDENTITY";
+    public static final String USER_REPLY_COMMENT = "USER_REPLY_COMMENT";
 }
