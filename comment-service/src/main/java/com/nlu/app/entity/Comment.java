97e16945fe1220bf3e85d2f8e7d53286d72c7765
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-02 20:32:50 +0700

    update: change logic about create new comment also publish event to outbox if necessary.

--- comment-service/src/main/java/com/nlu/app/entity/Comment.java
+++ comment-service/src/main/java/com/nlu/app/entity/Comment.java
@@ -6,11 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.UUID;
 
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -25,16 +21,8 @@ public class Comment {
     String videoId;
     String userId;
     String content;
-    Long timestamp;
+    LocalDateTime createAt;
+    LocalDateTime updateAt;
     String parentId;
-
     // Getters and Setters
-
-    public LocalDateTime getTimestamp() {
-        return LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneId.systemDefault());
-    }
-
-    public void setTimestamp(LocalDateTime localDateTime) {
-        this.timestamp = localDateTime.atZone(ZoneId.systemDefault()).toEpochSecond();
-    }
 }
--- comment-service/src/main/java/com/nlu/app/service/CommentService.java
+++ comment-service/src/main/java/com/nlu/app/service/CommentService.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nlu.app.common.dto.notification.CommentRepliedDTO;
 import com.nlu.app.common.event.comment_created.CommentCreationEvent;
+import com.nlu.app.common.share.SagaAction;
+import com.nlu.app.common.share.SagaAdvancedStep;
+import com.nlu.app.common.share.SagaStatus;
+import com.nlu.app.common.share.event.CommentReplyEvent;
 import com.nlu.app.dto.identity.request.TokenUserRequest;
 import com.nlu.app.dto.request.CommentCreationRequestDTO;
 import com.nlu.app.entity.Comment;
@@ -22,6 +26,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -42,47 +47,56 @@ public class CommentService {
                     comment.setUserId(userId);
                     comment.setContent(request.getContent());
                     comment.setVideoId(request.getVideoId());
-                    comment.setTimestamp(LocalDateTime.now());
+                    comment.setCreateAt(LocalDateTime.now());
+                    comment.setUpdateAt(LocalDateTime.now());
                     comment.setParentId(request.getParentId());
-                    return Mono.fromCallable(() -> _insertToDB_(userId, comment))
+                    return Mono.fromCallable(() -> _insertToDB_(comment))
                             .subscribeOn(Schedulers.boundedElastic());
                 })
                 .switchIfEmpty(Mono.error(new ApplicationException(ErrorCode.UNAUTHENTICATED)))
                 .map(value -> "OK");
     }
 
-    private Comment _insertToDB_(String userId, Comment comment) throws JsonProcessingException {
+    private Comment _insertToDB_(Comment comment) throws JsonProcessingException {
         boolean isNotification = false;
         Comment parentCmt = null;
+        String userId = comment.getUserId();
+        /* TODO:
+         *  Need to check first if this comment is worth for notification.
+         *  when a comment need to be notified, its mean it either send USER_REPLY_COMMENT
+         *  or USER_COMMENT_VIDEO events.
+         *  Then other services will catch the event and handle the rest.
+         */
         if (comment.getParentId() != null) {
             var o = commentRepository.findById(comment.getParentId());
             if (o.isEmpty()) throw new ApplicationException(ErrorCode.PARENT_COMMENT_NOT_EXISTED);
             parentCmt = o.get();
             isNotification = !parentCmt.getUserId().equalsIgnoreCase(userId); // Make no sense if notify about yourself
         }
+        commentRepository.save(comment);
 
         if (isNotification) {
 //                  In case comment is a reply, we need to publish its DTO to topic, using outbox pattern.
             ObjectMapper objectMapper = new ObjectMapper();
-//                    Create DTO
-            CommentRepliedDTO dto = new CommentRepliedDTO();
-            dto.setUserId(parentCmt.getUserId());
-            dto.setUserCommentId(parentCmt.getId());
-            dto.setReplierCommentId(comment.getId());
-            dto.setReplierId(comment.getUserId());
-            dto.setContent(comment.getContent());
-
-//                    Create Event object and warp DTO into it
-            CommentCreationEvent event = new CommentCreationEvent(dto);
+            var event = CommentReplyEvent.builder()
+                    .commentId(comment.getId())
+                    .content(comment.getContent())
+                    .userId(userId)
+                    .parentCommentId(parentCmt.getId())
+                    .parentUserId(parentCmt.getUserId())
+                    .build();
 //                    Insert to outbox table
-            Outbox outbox = new Outbox();
-            outbox.setAggregateId(parentCmt.getId());
-            outbox.setType("insert");
-            outbox.setAggregateType("replied");
-            outbox.setPayload(objectMapper.writeValueAsString(event));
+            Outbox outbox = Outbox.builder()
+                            .payload(objectMapper.writeValueAsString(event))
+                            .sagaId(comment.getId())
+                            .sagaAction(SagaAction.USER_REPLY_COMMENT)
+                            .sagaStepStatus(SagaStatus.SUCCESS)
+                            .sagaStep(SagaAdvancedStep.NOTIFICATION_CREATE)
+                            .aggregateId(comment.getId())
+                            .aggregateType("comment.topics")
+                            .build();
             outboxRepository.save(outbox);
         }
-        commentRepository.save(comment);
         return comment;
     }
 }
