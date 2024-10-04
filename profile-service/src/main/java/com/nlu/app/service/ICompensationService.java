cf452d6855bafda4f3a1879e368a3575deac1445
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-04 17:59:29 +0700

    refactor: add service interface for this service.

---
+++ profile-service/src/main/java/com/nlu/app/service/ICompensationService.java
@@ -0,0 +1,5 @@
+package com.nlu.app.service;
+
+public interface ICompensationService {
+    void doCompensation(String sagaId);
+}
---
+++ profile-service/src/main/java/com/nlu/app/service/IProfileService.java
@@ -0,0 +1,13 @@
+package com.nlu.app.service;
+
+import com.nlu.app.common.share.dto.profile_service.request.FollowRequest;
+import com.nlu.app.common.share.dto.profile_service.request.ProfileCreationRequest;
+import com.nlu.app.common.share.dto.profile_service.response.FollowerUserIdsResponse;
+import com.nlu.app.common.share.dto.profile_service.response.ProfileCreationResponse;
+import com.nlu.app.exception.ApplicationException;
+
+public interface IProfileService {
+    String follow(FollowRequest request) throws ApplicationException;
+    FollowerUserIdsResponse getFollowerIds(String userId) throws ApplicationException;
+    ProfileCreationResponse insert (ProfileCreationRequest request) throws ApplicationException;
+}
