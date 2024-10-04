bc8b0000a76430418d172a5f0eda2096a1d73f66
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-04 17:59:10 +0700

    refactor: add Mapper for this service

---
+++ profile-service/src/main/java/com/nlu/app/mapper/ProfileMapper.java
@@ -0,0 +1,25 @@
+package com.nlu.app.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nlu.app.common.share.SagaAction;
+import com.nlu.app.common.share.SagaAdvancedStep;
+import com.nlu.app.common.share.SagaStatus;
+import com.nlu.app.common.share.dto.profile_service.request.ProfileCreationRequest;
+import com.nlu.app.common.share.dto.profile_service.response.ProfileCreationResponse;
+import com.nlu.app.common.share.event.ProfileCreatedEvent;
+import com.nlu.app.common.share.event.ProfileRemovedEvent;
+import com.nlu.app.entity.Outbox;
+import com.nlu.app.entity.Profile;
+import com.nlu.app.exception.ApplicationException;
+import com.nlu.app.exception.ErrorCode;
+import org.mapstruct.*;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        builder = @Builder(disableBuilder = true))
+public interface ProfileMapper {
+    Profile toEntity(ProfileCreationRequest request);
+    ProfileCreatedEvent toProfileCreatedEvent(Profile profile);
+    ProfileRemovedEvent toProfileRemovedEvent(Profile profile);
+    ProfileCreationResponse toResponseDTO(Profile profile);
+}
