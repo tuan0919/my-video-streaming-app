8d4d01981918a2eb700cd25ad5c2abb6f55687fb
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-09 20:29:26 +0700

    new: adding caching with redis when generating video's URL.

--- video-streaming-service/src/main/java/com/nlu/app/dto/response/SignedURLResponse.java
+++ video-streaming-service/src/main/java/com/nlu/app/dto/response/SignedURLResponse.java
@@ -3,11 +3,13 @@ package com.nlu.app.dto.response;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class SignedURLResponse {
+public class SignedURLResponse implements Serializable {
     String link;
 }
