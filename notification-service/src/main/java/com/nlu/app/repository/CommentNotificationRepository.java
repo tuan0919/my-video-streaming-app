1d3538be9838287e88f9bb8979aa0e8e033c319b
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-13 17:46:22 +0700

    remove: removed unused classes.

--- notification-service/src/main/java/com/nlu/app/repository/CommentNotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nlu.app.repository;
-
-import com.nlu.app.entity.CommentNotification;
-import com.nlu.app.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentNotificationRepository extends JpaRepository<CommentNotification, String> {
-}
